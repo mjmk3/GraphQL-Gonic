0f9b69d069fc8b06ef924bf665a2b3c528f88c74
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-03-23 02:48:53 +0300

    GraphQL with Gin-Gonic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module GraphGonic
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,9 @@
+package GraphGonic
+
+/**
+ * @author MJ Makki
+ * @version 1.0
+ * @license SkyLimits, LLC (<a href="https://www.skylimits.tech">SkyLimits, LLC</a>)
+ * @email m.makki@skylimits.tech
+ * @since long time ago
+ */
